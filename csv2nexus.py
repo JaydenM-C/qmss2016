3ff1a3b6c9539f3dea236fcace38a46faf85c412
Tiago Tresoldi <tresoldi@gmail.com>
2016-05-15 16:37:40 +0200

    Added conversion to CSV data to NEXUS

---
+++ csv2nexus.py
@@ -0,0 +1,24 @@
+import csv
+import sys
+infile = sys.argv[1]
+
+with open(infile) as f:
+    reader = csv.reader(f)
+    cols = []
+    for row in reader:
+        cols.append(row)
+
+print "#NEXUS"
+print "Begin data;"
+print "Dimensions ntax=%i nchar=%i;" % (len(cols), len(cols[0]))
+print "Format datatype=Standard missing=? gap=-;"
+print "Matrix"
+for col in cols[1:]: # first line is headers
+	species = col[0]
+	morph = ''.join(col[1:])
+	morph = morph.replace('F', '0')
+	morph = morph.replace('T', '1')
+	print '%s %s' % (species, morph)
+
+print ";"
+print "End;"		
